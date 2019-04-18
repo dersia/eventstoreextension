9ef6f8a14a27701476f91f71c2bf07c80c86b5bc
Sia Ghassemi <extern.sia.ghassemi@vwfsdev.com>
2019-04-18 17:56:29 +0200

    fixed last JS errors. Added bunch of new Converters to support more scenarios

--- src/SiaConsulting.Azure.WebJobs.Extensions.EventStoreExtension.Streams/Config/EventStoreStreamsConfigProvider.cs
+++ src/SiaConsulting.Azure.WebJobs.Extensions.EventStoreExtension.Streams/Config/EventStoreStreamsConfigProvider.cs
@@ -7,9 +7,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using Microsoft.Azure.WebJobs;
-using System.Text;
-using System.Linq;
-using SiaConsulting.Azure.WebJobs.Extensions.EventStoreExtension.Streams.Exceptions;
+using SiaConsulting.Azure.WebJobs.Extensions.EventStoreExtension.Converter;
 
 namespace SiaConsulting.Azure.WebJobs.Extensions.EventStoreExtension.Streams.Config
 {
@@ -34,54 +32,12 @@ namespace SiaConsulting.Azure.WebJobs.Extensions.EventStoreExtension.Streams.Con
 
             context.AddConverter<string, JObject>(JObject.FromObject);
             context.AddConverter<JObject, ResolvedEvent?>(input => input.ToObject<ResolvedEvent?>());
-            context.AddConverter<JObject, EventData>(input => input.ToObject<EventData>());
             context.AddConverter<JObject, IList<ResolvedEvent>>(input => input.ToObject<IList<ResolvedEvent>>());
-            context.AddConverter<string, EventStoreData>(ConvertFromString);
-            context.AddConverter<IList<ResolvedEvent>, JArray>(JArray.FromObject);
-            context.AddConverter<IList<ResolvedEvent>, JObject>(JObject.FromObject);
+            context.AddAllConverters(_logger);
 
             var eventStoreAttributeRule = context.AddBindingRule<EventStoreStreamsAttribute>();
             eventStoreAttributeRule.BindToInput<IList<ResolvedEvent>>(new EventStoreInputAsyncConverter(_logger));
             eventStoreAttributeRule.BindToCollector<EventStoreData>(config => new EventStoreDataAsyncCollector(config, _logger));
-        }
-
-        private EventStoreData ConvertFromString(string data)
-        {
-            var tmpData = JObject.Parse(data);
-            var tmpEvents = tmpData["events"].ToObject<JArray>();
-            var convertedEvents = tmpEvents.Select(ConvertEvent);
-            return new EventStoreData(tmpData["streamName"]?.ToObject<string>(),tmpData["expectedVersion"]?.ToObject<long>() ?? ExpectedVersion.Any, convertedEvents);
-        }
-
-        private EventData ConvertEvent(JToken jToken)
-        {
-            var eventId = SafeParse(jToken["eventId"], JTokenType.Guid, Guid.NewGuid());
-            var eventType = SafeParse<string>(jToken["type"], JTokenType.String, null);
-            var eventIsJson = SafeParse(jToken["isJson"], JTokenType.Boolean, false);
-            var eventData = ToBytes(jToken["data"]);
-            var eventMetaData = ToBytes(jToken["metadata"]);
-            return new EventData(eventId, eventType, eventIsJson, eventData, eventMetaData);
-        }
-
-        private byte[] ToBytes(JToken data)
-        {
-            if (data == null || data.Type == JTokenType.Null)
-            {
-                return null;
-            }
-            if(data.Type == JTokenType.String)
-            {
-                return Encoding.UTF8.GetBytes(data.ToObject<string>());
-            }
-            var jsonObject = data.ToObject<JObject>();
-            return Encoding.UTF8.GetBytes(jsonObject.ToString());
-        }
-
-        private T SafeParse<T>(JToken token, JTokenType expectedJType, T defaultValue)
-        {
-            if (token == null || token.Type != expectedJType)
-                return defaultValue;
-            return token.ToObject<T>();
         }
     }
 }
